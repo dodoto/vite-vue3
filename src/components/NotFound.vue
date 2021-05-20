bc7a0aede02362d9074b4a7f0e6f1b1eaa9842eb
dodoto <279176202@qq.com>
2021-05-20 15:38:59 +0800

    404

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,5 +1,9 @@
 <template>
-  <p >it is 404</p>
+  <div class="out">
+    <div class="circle-line">
+    </div>
+    <div class="inner">404!</div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +11,34 @@ export default {
   name: 'NotFound',
 }
 </script>
+
+<style scoped>
+.out {
+  background-color: #98bec8;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+}
+.circle-line {
+  position: absolute;
+  top: 0; left: 0; bottom: 0; right: 0;
+  margin: auto;
+  width: 170px; height: 170px;
+  border-radius: 50%;
+  border: 3px dashed #4f7b86;
+  animation: rotate 20s linear infinite;
+}
+.inner {
+  position: absolute;
+  top: 0; left: 0; bottom: 0; right: 0;
+  margin: auto;
+  border-radius: 50%;
+  background-color: #4f7b86;
+  line-height: 150px;
+  width: 150px; height: 150px;
+  text-align: center;
+  color: #fff;
+  font-weight: 900;
+  font-size: 2.5rem;
+}
+</style>
